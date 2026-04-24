2c3ea5e8753ed6a42f4b7db1f75991ee503db9b9
Fabio Luna <fabioluna20@gmail.com>
2026-04-24 16:20:40 +0200

    new .v files

--- project_1/project_1.srcs/sources_1/new/shaper.v
+++ project_1/project_1.srcs/sources_1/new/shaper.v
@@ -20,7 +20,113 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module shaper(
+module shaper
+#(
+    parameter NBITS_IN = 33,
+    parameter integer FB = 15
 
-    );
+)
+(   
+    input clk,
+    input signed [NBITS_IN-1:0] x,
+    output signed [NBITS_IN+14:0] y
+);
+
+// FILTRO 1
+lattice_1ordem 
+#(
+    .NBITS_IN(33), 
+    .FB(15), 
+    .k(-32766), 
+    .v1(-8), 
+    .v2(8)
+) 
+lat1
+(
+    .clock(clk),
+    .in(x),
+    .out(y)
+);
+
+// FILTRO 2
+lattice_1ordem 
+#(
+    .NBITS_IN(33), 
+    .FB(15), 
+    .k(-19875), 
+    .v1(534729), 
+    .v2(881620)
+) 
+lat2
+(
+    .clock(clk),
+    .in(x),
+    .out(y)
+);
+
+// FILTRO 3
+lattice_1ordem 
+#(
+    .NBITS_IN(33), 
+    .FB(15), 
+    .k(-17983), 
+    .v1(-544293), 
+    .v2(-991767)
+) 
+lat3
+(
+    .clock(clk),
+    .in(x),
+    .out(y)
+);
+
+// FILTRO 4
+lattice_2ordem 
+#(
+    .NBITS_IN(33), 
+    .FB(15), 
+    .k1(842), 
+    .k2(14),
+    .v1(7916), 
+    .v2(-307901),
+    .v3(-875)
+) 
+lat4
+(
+    .clock(clk),
+    .in(x),
+    .out(y)
+);
+
+// FILTRO 5
+lattice_1ordem 
+#(
+    .NBITS_IN(33), 
+    .FB(15), 
+    .k(-221), 
+    .v1(1979), 
+    .v2(293709)
+) 
+lat5
+(
+    .clock(clk),
+    .in(x),
+    .out(y)
+);
+
+// FILTRO 6
+lattice_1ordem 
+#(
+    .NBITS_IN(33), 
+    .FB(15), 
+    .k(-221), 
+    .v1(1048), 
+    .v2(155530)
+) 
+lat6
+(
+    .clock(clk),
+    .in(x),
+    .out(y)
+);
 endmodule
