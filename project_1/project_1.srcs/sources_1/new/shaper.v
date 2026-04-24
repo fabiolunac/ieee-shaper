691e6c97b299a31abd08289d78e873e54ad0e4ad
Fabio Luna <fabioluna20@gmail.com>
2026-04-24 14:52:19 +0200

    project folder

---
+++ project_1/project_1.srcs/sources_1/new/shaper.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/24/2026 02:39:03 PM
+// Design Name: 
+// Module Name: shaper
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module shaper(
+
+    );
+endmodule
